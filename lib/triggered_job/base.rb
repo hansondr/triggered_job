30385086a90edc883ff81a9f45359bbf013823e5
Daniel Hanson <dan.hanson@americanfinancing.net>
2016-10-19 14:44:16 -0600

    Generalize interface

--- lib/triggered_job/base.rb
+++ lib/triggered_job/base.rb
@@ -1,10 +1,7 @@
 module TriggeredJob
   class Base
-    def self.triggered_by(table: nil, field: nil)
-      raise ArgumentError, ":table argument is required" if table.nil?
-      raise ArgumentError, ":field argument is required" if field.nil?
-
-      TriggeredJob.set_trigger([table, field].join("__"), self)
+    def self.triggered_by(trigger)
+      TriggeredJob.set_trigger(trigger, self)
     end
   end
 end
--- spec/lib/triggered_job/base_spec.rb
+++ spec/lib/triggered_job/base_spec.rb
@@ -3,36 +3,22 @@ require "support/constant_helpers"
 
 RSpec.describe TriggeredJob::Base do
   describe ".triggered_by" do
-    context "missing required attributes" do
-      it "raises exception when missing :table" do
-        begin
-          expect {
-            class A < TriggeredJob::Base
-              triggered_by field: "my_field"
-            end
-          }.to raise_error(ArgumentError, /table/)
-        ensure
-          remove_constants :A
-        end
-      end
-
-      it "raises exception when missing :field" do
-        begin
-          expect {
-            class A < TriggeredJob::Base
-              triggered_by table: "my_table"
-            end
-          }.to raise_error(ArgumentError, /field/)
-        ensure
-          remove_constants :A
-        end
+    it "raises exception when trigger is missing" do
+      begin
+        expect {
+          class A < TriggeredJob::Base
+            triggered_by
+          end
+        }.to raise_error ArgumentError
+      ensure
+        remove_constants :A
       end
     end
 
-    it "registers table+field trigger with itself" do
+    it "registers trigger with itself" do
       begin
         class A < TriggeredJob::Base
-          triggered_by table: "table", field: "field"
+          triggered_by "table__field"
         end
 
         expect(TriggeredJob.triggered_by("table__field")).to contain_exactly("A")
